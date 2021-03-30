9b5b2683a87990603170b4d71e58136bcfc091be
zullum <zullumsan@gmail.com>
2021-03-30 09:23:33 +0200

    Added Order model controller and route

---
+++ database/migrations/2021_03_30_071413_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('quantity');
+            $table->decimal('price');
+            $table->string('status');
+            $table->integer('store_id');
+            $table->integer('owner_id');
+            $table->integer('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\UserController;
@@ -24,3 +25,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('store', StoreController::class);
 Route::apiResource('user', UserController::class);
 Route::apiResource('product', ProductController::class);
+Route::apiResource('order', OrderController::class);
