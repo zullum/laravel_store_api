a3a5c575f1b68664dc2bbdbd3267b40c74aac8b8
zullum <zullumsan@gmail.com>
2021-03-30 19:31:05 +0200

    Added CRUD operations for Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Store;
 use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -14,7 +18,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return ['response' => 'index for product'];
+        return Product::all();
     }
 
     /**
@@ -35,7 +39,51 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = Auth::user();
+        $user_id = $user->id;
+        $user_ar = $user->toArray();
+        $user_validator = Validator::make($user_ar, [
+            'role' => 'required|in:manager',
+        ]);
+        if ($user_validator->fails()) {
+            return response()->json(['error'=>$user_validator->errors()], 400);
+        }
+
+        $stores = Store::where('manager_id', $user_id)->get()->pluck('id')->toArray();
+
+        $input_validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'color' => 'required|string',
+            'type' => 'required|string',
+            'size' => 'required|string',
+            'material' => 'required|string',
+            'price' => 'required|numeric',
+            'quantity_in_stock' => 'required|integer',
+            // 'store_id' => 'required|integer|in:'.$stores,
+            'store_id' => [
+                'required',
+                'integer',
+                Rule::in($stores),
+            ],
+        ]);
+        if ($input_validator->fails()) {
+            return response()->json(['error'=>$input_validator->errors()], 400);
+        }
+        $product = new Product;
+        $product->name = $request->name;
+        $product->color = $request->color;
+        $product->type = $request->type;
+        $product->size = $request->size;
+        $product->material = $request->material;
+        $product->price = $request->price;
+        $product->quantity_in_stock = $request->quantity_in_stock;
+        $product->store_id = $request->store_id;
+        $result = $product->save();
+        if($result) {
+            return response()->json(['response'=>'Product created succesfully'], 201);
+        }
+        return response()->json(['response'=>'Operation store failed'], 400);
+
     }
 
     /**
@@ -46,7 +94,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return $product;
     }
 
     /**
@@ -69,7 +117,47 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $user = Auth::user();
+        $user_id = $user->id;
+        $user_ar = $user->toArray();
+        $user_validator = Validator::make($user_ar, [
+            'role' => 'required|in:manager',
+        ]);
+        if ($user_validator->fails()) {
+            return response()->json(['error'=>$user_validator->errors()], 400);
+        }
+
+        $stores = Store::where('manager_id', $user_id)->get()->pluck('id')->toArray();
+
+        $input_validator = Validator::make($request->all(), [
+            'name' => array_key_exists('name', $request->all()) ? 'required|string' : '',
+            'color' => array_key_exists('color', $request->all()) ?  'required|string' : '',
+            'type' => array_key_exists('type', $request->all()) ?  'required|string' : '',
+            'size' => array_key_exists('size', $request->all()) ?  'required|string' : '',
+            'material' => array_key_exists('material', $request->all()) ?  'required|string' : '',
+            'price' => array_key_exists('price', $request->all()) ?  'required|numeric' : '',
+            'quantity_in_stock' => array_key_exists('quantity_in_stock', $request->all()) ?  'required|integer' : '',
+            'store_id' => [
+                'required',
+                'integer',
+                Rule::in($stores),
+            ],
+        ]);
+        if ($input_validator->fails()) {
+            return response()->json(['error'=>$input_validator->errors()], 400);
+        }
+
+        foreach ($request->all() as $key => $value) {
+            if($key != 'store_id') {
+                $product->{$key} = $value;
+            }
+        }
+
+        $result = $product->save();
+        if($result) {
+            return response()->json(['response'=>'Product updated succesfully'], 201);
+        }
+        return response()->json(['response'=>'Operation update failed'], 400);
     }
 
     /**
@@ -80,6 +168,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $result = $product->delete();
+        if($result) {
+            return response()->json(['response'=>'Product deleted succesfully'], 201);
+        }
+        return response()->json(['response'=>'Operation delete failed'], 400);
     }
 }
