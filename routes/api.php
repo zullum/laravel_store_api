7f25fb1d4334391b73d643c7cb013154f419905d
zullum <zullumsan@gmail.com>
2021-03-30 08:39:44 +0200

    Added UserControler and route for user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StoreController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('store', StoreController::class);
+Route::apiResource('user', UserController::class);
